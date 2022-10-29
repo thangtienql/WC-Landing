2108ac3a69dcf404da0a1a2d6897d17bf0057a76
tienhtpd02242 <48255077+thangtienql@users.noreply.github.com>
2022-10-29 23:20:10 +0700

    fix bug
    fix bug

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,4 +10,22 @@
 
     AOS.init();
 
+    $('.main-header .__toggle-btn').click( function() {
+        // $(this).addClass('active');
+        $('body').addClass('overplay');
+        $('.__menu-mobile').addClass('active');
+    });
+
+    $('.__menu-mobile .close-icon').click( function() {
+        $('body').removeClass('overplay');
+        $('.__menu-mobile').removeClass('active');
+    });
+
+    $(document).mouseup(function(e) {
+        if (!$(e.target).is(".__menu-mobile")) {
+            $('body').removeClass('overplay');
+            $('.__menu-mobile').removeClass('active');
+        }
+    });
+
  })( jQuery );
