0d61651bf262e9ae42946aa655a3496a7e71df16
tienhtpd02242 <48255077+thangtienql@users.noreply.github.com>
2022-10-30 11:50:46 +0700

    udate
    udate

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,4 +28,17 @@
         }
     });
 
+    $('a[href*=\\#]:not([href=\\#])').on('click', function(){
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: (target.offset().top)
+                }, 700);
+                return false;
+            }
+        }
+    });
+
  })( jQuery );
